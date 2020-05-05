904ebc0de0decda16bec32c48f78bdd8892ebef1
Wagner de Lima <waglds@gmail.com>
2020-05-05 18:57:14 +0200

    organising the files and code

--- etl.py
+++ etl.py
@@ -1,19 +1,30 @@
+import os
 import configparser
 from datetime import datetime
-import os
+
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, col
-from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
-
+from pyspark.sql.functions import (
+    year,
+    month,
+    dayofmonth,
+    hour,
+    weekofyear,
+    date_format
+)
 
 config = configparser.ConfigParser()
 config.read('dl.cfg')
 
-os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
-os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
+
+os.environ['AWS_ACCESS_KEY_ID'] = config['AWS']['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY'] = config['AWS']['AWS_SECRET_ACCESS_KEY']
 
 
 def create_spark_session():
+    """
+    Fetches or creates a spark session.
+    """
     spark = SparkSession \
         .builder \
         .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
