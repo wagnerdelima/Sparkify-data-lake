dc8d58af81797ace8f6f3084c83c9efd03ac7998
Wagner de Lima <waglds@gmail.com>
2020-05-06 13:00:17 +0200

    included bucket

--- etl.py
+++ etl.py
@@ -168,7 +168,7 @@ def process_log_data(spark, input_data, output_data):
 def main():
     spark = create_spark_session()
     input_data = 's3a://udacity-dend/'
-    output_data = ''
+    output_data = 's3a://waglds-lake-bucket/'
 
     process_song_data(spark, input_data, output_data)
     process_log_data(spark, input_data, output_data)
